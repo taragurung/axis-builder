1dd20517923cc411a26afd7a47ecbefbe49366b8
shivapoudel <axisthemes@gmail.com>
2014-11-16 01:01:18 +0545

    Theme name fix for unit test

--- tests/unit-tests/core-functions.php
+++ tests/unit-tests/core-functions.php
@@ -13,7 +13,7 @@ class AB_Tests_Core_Functions extends AB_Unit_Test_Case {
 	 */
 	public function test_ab_get_core_supported_themes() {
 
-		$expected_themes = array( 'twentyfourteen', 'twentythirteen', 'twentyeleven', 'twentytwelve', 'twentyten' );
+		$expected_themes = array( 'twentyfifteen', 'twentyfourteen', 'twentythirteen', 'twentyeleven', 'twentytwelve', 'twentyten' );
 
 		$this->assertEquals( $expected_themes, ab_get_core_supported_themes() );
 	}
