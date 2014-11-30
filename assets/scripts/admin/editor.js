e562d46f3ea7b21dca1e1713f854e3293f7c16cb
shivapoudel <axisthemes@gmail.com>
2014-11-30 20:58:45 +0545

    TinyMCE Button for Builder Loaded

---
+++ assets/scripts/admin/editor.js
@@ -0,0 +1,36 @@
+/*global tinymce */
+
+( function () {
+
+	/**
+	 * Check is empty.
+	 *
+	 * @param  {string} value
+	 * @return {bool}
+	 */
+	function axisShortcodesIsEmpty( value ) {
+		value = value.toString();
+
+		if ( 0 !== value.length ) {
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * Add the shortcodes downdown.
+	 */
+	tinymce.PluginManager.add( 'axisbuilder_shortcodes', function ( editor ) {
+		var ed = tinymce.activeEditor;
+		editor.addButton( 'axisbuilder_shortcodes', {
+			title : ed.getLang( 'axisbuilder_shortcodes.shortcode_title' ),
+			text: ed.getLang( 'axisbuilder_shortcodes.shortcode_title' ),
+			icon: 'axisbuilder-shortcodes',
+			type: 'menubutton',
+			menu: [
+
+			]
+		});
+	});
+})();
--- includes/admin/class-builder-admin-editor.php
+++ includes/admin/class-builder-admin-editor.php
@@ -24,25 +24,24 @@ class AB_Admin_Editor {
 	 * Hook in tabs.
 	 */
 	public function __construct() {
-		add_action( 'admin_init', array( $this, 'init' ) );
+		add_action( 'admin_init', array( $this, 'add_editor_button' ) );
+		add_action( 'admin_init', array( $this, 'add_shortcode_button' ) );
+		add_filter( 'tiny_mce_version', array( $this, 'refresh_tiny_mce' ) );
+		add_filter( 'mce_external_languages', array( $this, 'add_tinymce_locales' ), 20, 1 );
 	}
 
 	/**
-	 * Init Editor
+	 * Add a button for toggle between the editors.
 	 */
-	public function init() {
-		// Add Toggle Button
-		$this->apply_editor_wrap();
-	}
-
-	/**
-	 * Apply Editor Wrap
-	 */
-	public function apply_editor_wrap() {
+	public function add_editor_button() {
 		add_action( 'edit_form_after_title', array( $this, 'wrap_default_editor' ) );
 		add_action( 'edit_form_after_editor', array( $this, 'close_default_editor_wrap' ) );
 	}
 
+	/**
+	 * wrap_default_editor.
+	 * @return string
+	 */
 	public function wrap_default_editor() {
 		$screen = get_current_screen();
 
@@ -61,6 +60,10 @@ class AB_Admin_Editor {
 		}
 	}
 
+	/**
+	 * close_default_editor_wrap.
+	 * @return string
+	 */
 	public function close_default_editor_wrap() {
 		$screen = get_current_screen();
 
@@ -68,6 +71,66 @@ class AB_Admin_Editor {
 			echo '</div> <!-- #postdivrich_wrap -->';
 		}
 	}
+
+	/**
+	 * Add a button for shortcodes to the WP editor.
+	 */
+	public function add_shortcode_button() {
+		if ( ! current_user_can( 'edit_posts' ) && ! current_user_can( 'edit_pages' ) ) {
+			return;
+		}
+
+		if ( 'true' == get_user_option( 'rich_editing' ) ) {
+			add_filter( 'mce_buttons', array( $this, 'register_shortcode_button' ) );
+			add_filter( 'mce_external_plugins', array( $this, 'add_shortcode_tinymce_plugin' ) );
+		}
+	}
+
+	/**
+	 * Register the shortcode button.
+	 * @param  array $buttons
+	 * @return array $buttons
+	 */
+	public function register_shortcode_button( $buttons ) {
+		array_push( $buttons, '|', 'axisbuilder_shortcodes' );
+
+		return $buttons;
+	}
+
+	/**
+	 * Add the shortcode button to TinyMCE.
+	 * @param  array $plugins TinyMCE plugins.
+	 * @return array $plugins AxisBuilder TinyMCE plugin.
+	 */
+	public function add_shortcode_tinymce_plugin( $plugins ) {
+		$suffix = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
+
+		$plugins['axisbuilder_shortcodes'] = AB()->plugin_url() . '/assets/scripts/admin/editor' . $suffix . '.js';
+
+		return $plugins;
+	}
+
+	/**
+	 * Force TinyMCE to refresh.
+	 * @param  int $version
+	 * @return int
+	 */
+	public function refresh_tiny_mce( $version ) {
+		$version += 3;
+
+		return $version;
+	}
+
+	/**
+	 * TinyMCE locales function.
+	 * @param  array $locales TinyMCE locales.
+	 * @return array
+	 */
+	public function add_tinymce_locales( $locales ) {
+		$locales['axisbuilder_shortcodes'] = AB()->plugin_path() . '/i18n/shortcodes.php';
+
+		return $locales;
+	}
 }
 
-return new AB_Admin_Editor();
+new AB_Admin_Editor();
