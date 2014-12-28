59c08c83fef2ef87a3d6615e9c6da4812ffd4c65
shivapoudel <axisthemes@gmail.com>
2014-12-28 23:32:55 +0545

    OH! this is causing the load of all shortcodes

--- includes/class-builder-shortcodes.php
+++ includes/class-builder-shortcodes.php
@@ -170,7 +170,7 @@ class AB_Shortcodes {
 
 		if ( sizeof( $this->shortcodes ) > 0 ) {
 			foreach ( $this->shortcodes as $load_shortcodes ) {
-				if ( empty( $load_shortcodes->shortcode['tinymce']['disable'] ) ) {
+				if ( empty( $load_shortcodes->shortcode['tinyMCE']['disable'] ) ) {
 					$_available_shortcodes[ $load_shortcodes->shortcode['name'] ]['type']    = $load_shortcodes->shortcode['type'];
 					$_available_shortcodes[ $load_shortcodes->shortcode['name'] ]['title']   = $load_shortcodes->title;
 					$_available_shortcodes[ $load_shortcodes->shortcode['name'] ]['tinyMCE'] = $load_shortcodes->shortcode['tinyMCE'];
