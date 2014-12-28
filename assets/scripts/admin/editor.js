65fe8812f7eab38d2ff46717dccecf16d29a908a
shivapoudel <axisthemes@gmail.com>
2014-12-28 22:01:29 +0545

    Some stage of tinyMCE plugin for builder

--- assets/scripts/admin/editor.js
+++ assets/scripts/admin/editor.js
@@ -24,11 +24,11 @@
 				text: ed.getLang( 'axisbuilder_shortcodes.shortcode_text' ),
 				icon: 'axisbuilder-shortcodes',
 				type: 'menubutton',
-				menu: self.createMenu( editor )
+				menu: self.createMenu()
 			});
 
 			editor.addCommand( 'Open_AxisBuilderModal', function( ui, params ) {
-				var modal = new $.AxisBuilderModal( params );
+				// var modal = new $.AxisBuilderModal( params );
 				return false;
 			});
 		},
@@ -36,14 +36,83 @@
 		/**
 		 * Structure the Menu.
 		 */
-		createMenu: function( editor ) {
-			var self            = this,
-				counter         = 0,
-				submenu         = [],
-				final_options   = [],
-				open_modal      = $.AxisBuilderModal.openInstance || [];
-				// shortcode_array = axisbuilder_globals.shortcode['axisbuilder_shortcodes'].config;
+		createMenu: function() {
+			var ed         = tinymce.activeEditor,
+				modal      = $.AxisBuilderModal.openInstance || [],
+				shortcodes = ed.getLang( 'axisbuilder_shortcodes.shortcodes' ),
+				title, dropdown, self = this, tabs = [], submenu = [], loop = 0;
 
+			// Get all tabs
+			for ( var i in shortcodes ) {
+				tabs[shortcodes[i].type] = [];
+			}
+
+			// Create the sub-menus
+			for ( title in tabs ) {
+				if ( title !== 'undefined' ) {
+					loop++;
+					submenu.push({
+						text: ed.getLang( 'axisbuilder_shortcodes.' + title + '_label' ),
+						menu: []
+					});
+				}
+			}
+
+			// Add items to sub-menus.
+			for ( dropdown in shortcodes ) {
+
+				// Set default
+				shortcodes[dropdown].tinyMCE = shortcodes[dropdown].tinyMCE || {};
+
+				// Only render subset of elements if backbone modal is open
+				// if ( modal.length == 0 && ! shortcodes[dropdown].type || typeof shortcodes[dropdown].tinyMCE.tiny_always !== 'undefined' ) {
+
+					var current = submenu, paramText = '', paramOnClick = '';
+
+					for ( title in submenu ) {
+						if ( title !== 'undefined' ) {
+							var text = ed.getLang( 'axisbuilder_shortcodes.' + shortcodes[dropdown].type + '_label' );
+
+							if ( submenu[title].text === text ) {
+								current = submenu[title].menu;
+							}
+						}
+					}
+
+					paramText    = shortcodes[dropdown].tinyMCE.name || shortcodes[dropdown].title;
+					paramOnClick = ( typeof shortcodes[dropdown].tinyMCE.instantInsert !== 'undefined' ) ? self.instantInsert : self.modalInsert;
+
+					current.push({
+						text: paramText,
+						onClick: paramOnClick,
+						axisbuilder_shortcode: shortcodes[dropdown]
+					});
+
+				// }
+			}
+
+			// Remove empty sub-menus
+			for ( title in submenu ) {
+				if ( ( title !== 'undefined' ) && ( typeof submenu[title].menu !== 'undefined' ) ) {
+					if ( submenu[title].menu.length === 0 ) {
+						delete( submenu[title] );
+					}
+				}
+			}
+
+        	return submenu;
+		},
+
+		instantInsert: function() {
+			var ed        = tinymce.activeEditor,
+				shortcode = this.settings.axisbuilder_shortcode;
+
+			// Execute command ;)
+			ed.execCommand( 'mceInsertContent', false, window.switchEditors.wpautop( shortcode.tinyMCE.instantInsert ) );
+		},
+
+		modalInsert: function() {
+			// Load Backbone Modal soon ;)
 		}
 	});
 
