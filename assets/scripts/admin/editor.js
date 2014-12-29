dc1e09226c0696c82c6f4b49e8c69f8cea83a82f
shivapoudel <axisthemes@gmail.com>
2014-12-29 17:49:14 +0545

    Not sure if we can use this too

--- assets/scripts/admin/editor.js
+++ assets/scripts/admin/editor.js
@@ -107,6 +107,7 @@
 
 			// Execute command ;)
 			ed.execCommand( 'mceInsertContent', false, window.switchEditors.wpautop( shortcode.tinyMCE.instantInsert ) );
+			// ed.insertContent( shortcode.tinyMCE.instantInsert );
 		},
 
 		modalInsert: function() {
